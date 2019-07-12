3e51439e19c2de1f8142c142b52e24aee6429585
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 20:17:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,12 +2,11 @@ def square_array(array)
 
   counter = 0
  new_array = []
-  while < array[] do
+  while counter < array[] do
     puts array[]
     array.push += 1 ** 2
   end
 end
 
-#array = [1,2,3]
 
 
